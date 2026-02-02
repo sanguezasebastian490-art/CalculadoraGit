2ad0f0368704aa1fbeba7f8ce8c61c3f5bf7a059
Sebastian Sangueza <sanguezasebastian490@gmail.com>
2026-02-01 22:32:02 -0400

    Funcionalidad suma implementada

--- calculadora.cs
+++ calculadora.cs
@@ -3,5 +3,14 @@ using System;
 class Program {
     static void Main() {
         Console.WriteLine("Bienvenido a la Calculadora Básica");
+        
+        // Funcionalidad de suma
+        Console.Write("Ingrese primer número: ");
+        int a = int.Parse(Console.ReadLine());
+        
+        Console.Write("Ingrese segundo número: ");
+        int b = int.Parse(Console.ReadLine());
+        
+        Console.WriteLine($"Resultado: {a + b}");
     }
 }
